9adb9a9c84d188379dbcc018e0baf86c705a78da
Dan Stover <dstover1688@gmail.com>
2017-07-24 08:42:03 -0400

    script update
    added in some sorting logic that may work and did some formatting

---
+++ psudocode.py
@@ -0,0 +1,41 @@
+#coin[s] as param
+#loop through switch statement
+#>>> Do we put the Switch statement here or would we want to keep that logic in the getStats function to return any coins value
+##different api's required for some coins
+#calculate which coin to mine
+##coin to mine=fastest to target coin stack size in 30d?
+
+def calculateCoins([string[]]coins){
+  
+  $coinObject()
+  
+  foreach (coin in coins){
+  
+    $coinValue()
+  
+    switch coin {
+      'etc' = {
+        #Code for ETC, return coins/day
+      }
+  
+      'ubq' = {
+        #Code for UBIQ, return coins/day
+      }
+  
+      default = {
+      #Code to scrape API's for $coin, return coins/day
+      }
+  
+  }
+ 
+ #create resultant object
+ coinObject+=coinValue
+ 
+  }
+ #If object is JSON, we should be able to do a sort by property "CoinProfitability" for all objects and return the highest value. 
+ # To do this we should make sure we are storing the values of CoinProf as a Float type in all of the objects and we can use something like JSON sort to get the best coin.
+ #do math to sort coins here
+ coinObjectSorted = coinObject.sort_keys
+
+#  print('Sorted Coin Object' = ' + json.dumps(data, sort_keys=True) + ';')
+}
